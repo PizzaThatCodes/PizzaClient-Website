2befdbdc249606e9e0f27b475d8f646d6b9f4ea2
BuildTools <unconfigured@null.spigotmc.org>
2020-08-05 17:23:57 -0400

    3

--- index.php
+++ index.php
@@ -1,62 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
-  
+
   <head>
     <title>PizzaClient Home</title>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-    <!-- import the webpage's stylesheet -->
-    <link rel="stylesheet" href="/style.css" />
-
-    <!-- import the webpage's javascript file -->
-    <script src="/script.js" defer></script>
+    <link rel="stylesheet" type="text/css" href="assets/style.css">
   </head>
-  <body>
-    <nav class="header" id="MyHeader">
-      <img
-        src="https://media.discordapp.net/attachments/683632771033464845/705354220878037052/old-icon.png"
-        class="profile-img"
-      />
-      <li><a href="index.php">Home</a></li>
-      <li><a href="download.html">Download</a></li>
-      
-    </nav>
-    <h1>
-      Welcome to PizzaClient
-    </h1>
 
-               <form class="container" action="index.html" method="post">
-      <a name="about" style="position:relative; top: 0px; display:block;"></a>
-      <h1>About the Client</h1>
-      <h4>PizzaClient is one of the best fun Clients made out there for minecraft version 1.8.8</h4>
-      <ul style="padding-top: 50px;">
-        <h4 style="font-weight: bold;">Mods and Features</h4>
-        <about>Optifine</about>
-        <about>Keystrokes</about>
-        <about>Cosmetics</about>
-        <about>Custom Gui</about>
-        <about>And many more!</about>
-      </ul>
-    </form>
-    
-    <script>
-      window.onscroll = function() {
-        myFunction();
-      };
+    <body class="noselect">
 
-      var header = document.getElementById("MyHeader");
-      var sticky = header.offsetTop;
+        <div class="container">
+            <div class="content">
+                <div class="process">
 
-      function myFunction() {
-        if (window.pageYOffset > sticky) {
-          header.classList.add("sticky");
-        } else {
-          header.classList.remove("sticky");
-        }
-      }
-    </script>
-    
+                </div>
+            </div>
+
+        </div>
   </body>
 </html>
