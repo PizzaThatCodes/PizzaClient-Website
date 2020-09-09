e674f4433dca2a858dc3f72c87e3576043a81151
PizzaThatCodes <61859244+PizzaThatCodes@users.noreply.github.com>
2020-09-09 17:41:08 -0400

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,19 @@
     <link rel="stylesheet" type="text/css" href="style.css">
     <title>PizzaClient</title>
      <link rel="shortcut icon" href="website-icon.ico" />
-    <meta property="og:image" content="https://pizzaclient-website-pizzalover.herokuapp.com/Pizza%20Image%20-%20Copy.png">
     
     
-            <meta name="description" content="PizzaClient Download">
+    
+      <meta name="description" content="Home • PizzaClient">
+  <meta name="language" content="English">
+  <meta name="author" content="PizzaLover">
+  <meta name="theme-color" content="#fcd303">
+
+  <meta property="og:type" content="website">
+  <meta property="og:description" content="Home • PizzaClient">
+  <meta property="og:title" content="Home • pizzaclient.us.to">
+  <meta property="og:url" content="http://pizzaclient.us.to">
+  <meta property="og:image" content="https://pizzaclient-website-pizzalover.herokuapp.com/Pizza%20Image%20-%20Copy.png">
 </head>
 
 <body class="noselect">
